5b31ca6264f153b8953c864f06a6084a9e58d974
Jackelyn Velez <jlauravelez@gmail.com>
2020-06-13 09:58:23 +0200

    Polishing up Header

--- src/containers/Heading.tsx
+++ src/containers/Heading.tsx
@@ -1,14 +1,16 @@
 import React, { Component } from "react"
-import classes from "./Heading.module.css"
+import classes from "./Heading.module.css";
 import icon from "../assets/hbgr.svg";
 class Heading extends Component {
     render() {
         return (
             <div className={classes.container}>
                 <p className={classes.Location}>Location: <p className={classes.Bold}>Wrocław, Poland</p></p>
-                <p className={classes.Title}>JACKIE VELEZ</p>
-
-                <img src={icon} className = {classes.Ham} alt="drop down menu icon" />
+                <div >
+                    <p className={classes.Title}>JACKIE VELEZ</p>
+                    <p>Programmer, Educator, Creator</p>
+                </div>
+                <img src={icon} className={classes.Ham} alt="drop down menu icon" />
             </div>
         )
     }
