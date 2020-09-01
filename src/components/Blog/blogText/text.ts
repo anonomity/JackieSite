cd97ce3380aebe729633813b962158e38e95c3d0
Jackelyn Velez <jlauravelez@gmail.com>
2020-09-01 13:33:16 +0200

    added date field and new blog entry

--- src/components/Blog/blogText/text.ts
+++ src/components/Blog/blogText/text.ts
@@ -6,3 +6,13 @@ export const text12 =  `I know what you're probably thinking: 'omg ur a noob', a
 but not fully, I know that there are some hidden corners of the words that I have yet to investigate, other words I have a jumbled understanding of and I need to 
 do some serious maintenance work and weed out the discrepencies, anyways I hope these post can help those who know humility enought to ask the same questions and 
 seek the answers. Enjoy and buckle up, the journey ahead will not be simple.`
+
+export const text2 =   `
+So currently while creating these blog posts I'm trying to do it in the most efficient matter <obviously> and I'm starting to rethink my life, So right now I'm at the point 
+where I feel fairly confident in my React skills, my so called base, I know Javascript should be my base but for the time being, its sufficient. Due to my newfound confidence In my React skills, 
+I believe it's time to branch out, and work on different methods for creating React applications, other than create-react-app. When browsing, about what tech stack to use when creating
+efficient blog post application I kept being led to Gatsby. I knew sooner or later I would use this technology, now with a goal in mind I'm starting to feel excited. It's always exciting for
+me to learn new technologies for developing software and so far I'm enjoying this little documentation here of my journey, in retrospect, I think it's a pitty I didn't document the steps
+which led me to this spot more thoroughly, you take for granted the mountains you've climbed and the diversities faced. Anyways, I know the journey is not over and there is a lot ahead,
+hopefully someone benefits from this mumbo jumbo.
+`
