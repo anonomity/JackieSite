59e6932f4731807332e34e34c9ef066725bd0266
anonomity <jlauravelez@gmail.com>
2021-01-27 19:37:53 +0100

    deleting unused files, fix type errors

--- src/containers/Layout/Layout.tsx
+++ src/containers/Layout/Layout.tsx
@@ -1,19 +1,14 @@
-import React, {Component} from "react";
-import Heading from "../../components/Navbar/Heading"
+import React, { Component } from "react";
 
 import classes from "./Layout.module.css";
 class Layout extends Component {
-    
-    render(){
-
-        return(
-            <div className={classes.body}>
-               
-                <hr />
-                <main className={classes.content}>{this.props.children}</main>
-         
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={classes.body}>
+        <hr />
+        <main className={classes.content}>{this.props.children}</main>
+      </div>
+    );
+  }
 }
-export default Layout
+export default Layout;
