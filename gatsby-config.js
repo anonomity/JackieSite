9a1955e267f0a3fdc229d1c4accb913a9edf4cee
anonomity <jlauravelez@gmail.com>
2021-02-23 14:34:46 +0100

    adding favicon 🖼

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,14 @@ module.exports = {
     description: `My Programming Website`,
     author: `Jackelyn Velez`,
   },
-   plugins: [
+  plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/BlogPosts`
-      }
+        path: `${__dirname}/src/pages/BlogPosts`,
+      },
     },
     `gatsby-plugin-mdx`,
     {
@@ -21,5 +21,17 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
+      },
+    },
   ],
-}
+};
