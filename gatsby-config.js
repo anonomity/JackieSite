6d80f4f6094fb4452a9e3d359b6e027e008f6acb
anonomity <jlauravelez@gmail.com>
2021-03-03 21:25:42 +0100

    switching to yarn + adding dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,9 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
