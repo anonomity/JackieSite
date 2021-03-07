7e05ab30054a533183b662eac26803aeec3c12e1
anonomity <jlauravelez@gmail.com>
2021-03-07 17:45:37 +0100

    description of the garden

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -8,40 +8,49 @@ export interface IPostProps {
 const Posts = ({ data }: IPostProps) => {
   const { edges } = data.allMdx;
   return (
-    <div
-      style={{
-        marginTop: 100,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-around",
-      }}
-    >
-      {edges.map((edge) => {
-        const { frontmatter } = edge.node;
+    <div>
+      <p>
+        So the three different rankings [🌱, 🌿, 🌳] are for the development of
+        the post. When a post is new it gets the 🌱, it doesn't upgrade into 🌿
+        untill I have all the research written down. The final stage of
+        development 🌳 is achieved when I add all the pictures, and I organize
+        the text and edit, the typos and style.
+      </p>
+      <div
+        style={{
+          marginTop: 100,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        {edges.map((edge) => {
+          const { frontmatter } = edge.node;
 
-        return (
-          <Link
-            key={frontmatter.title}
-            to={frontmatter.path}
-            style={{ textDecoration: "none" }}
-          >
-            <div className={classes.box}>
-              <p
-                style={{
-                  fontWeight: "bold",
-                  textAlign: "center",
-                }}
-              >
-                {frontmatter.title} {frontmatter.status}
-              </p>
+          return (
+            <Link
+              key={frontmatter.title}
+              to={frontmatter.path}
+              style={{ textDecoration: "none" }}
+            >
+              <div className={classes.box}>
+                <p
+                  style={{
+                    fontWeight: "bold",
+                    textAlign: "center",
+                  }}
+                >
+                  {frontmatter.title} {frontmatter.status}
+                </p>
 
-              <p style={{ color: "#ccc", textAlign: "center" }}>
-                {frontmatter.date}
-              </p>
-            </div>
-          </Link>
-        );
-      })}
+                <p style={{ color: "#ccc", textAlign: "center" }}>
+                  {frontmatter.date}
+                </p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
